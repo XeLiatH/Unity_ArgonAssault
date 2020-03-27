9f8ffc0fe647eeefbbca29e25f14e5cbf2351bed
Matěj Beran <matej.beran96@gmail.com>
2020-03-27 19:26:21 +0100

    Utilize the user input to move the ship.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,9 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("In meters per second")] [SerializeField] float xSpeed = 4.0f;
+    [Tooltip("In meters")] [SerializeField] float xRange = 2.0f;
+
     void Start()
     {
         
@@ -12,7 +15,10 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xRaw = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
+
+        transform.localPosition = new Vector3(xRaw, transform.localPosition.y, transform.localPosition.z);
     }
 }
