a6b81ef0d142c6e9e4df4469179a850c5d8150aa
Matěj Beran <matej.beran96@gmail.com>
2020-03-28 19:24:30 +0100

    Implement better controlling of the player.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,24 +5,47 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In meters per second")] [SerializeField] float xSpeed = 4.0f;
+    [Tooltip("In meters per second")] [SerializeField] float xSpeed = 6.0f;
     [Tooltip("In meters")] [SerializeField] float xRange = 2.5f;
 
-    [Tooltip("In meters per second")] [SerializeField] float ySpeed = 4.0f;
+    [Tooltip("In meters per second")] [SerializeField] float ySpeed = 6.0f;
     [Tooltip("In meters")] [SerializeField] float yRange = 2.5f;
+
+    [SerializeField] float positionPitchFactor = -5.0f;
+    [SerializeField] float positionYawFactor = 5.0f;
+
+    [SerializeField] float controlPitchFactor = -30.0f;
+    [SerializeField] float controlRollFactor = -15.0f;
+
+    float xThrow, yThrow;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float xRaw = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
 
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * ySpeed * Time.deltaTime;
         float yRaw = Mathf.Clamp(transform.localPosition.y + yOffset, -yRange, yRange);
 
