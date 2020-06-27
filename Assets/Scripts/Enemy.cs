eb8587d66a17125cc61f8ce6c68adb74820fb228
Matěj Beran <matej.beran96@gmail.com>
2020-06-27 16:59:09 +0200

    Add box collider to enemies in runtime.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
     void OnParticleCollision(GameObject other)
     {
         Destroy(gameObject);
+    }
+
+    void AddNonTriggerBoxCollider()
+    {
+        Collider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
     }
 }
