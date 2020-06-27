546ec353b78250793987495b6b966fdb71112172
Matěj Beran <matej.beran96@gmail.com>
2020-06-27 15:12:16 +0200

    Add collisions to the player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,6 +24,11 @@ public class Player : MonoBehaviour
 
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        print("Player collided with something");
+    }
+
     void Update()
     {
         ProcessTranslation();
