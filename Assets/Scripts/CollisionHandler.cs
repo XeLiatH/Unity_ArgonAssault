5eb5fabc17d937a628e9430d9ac92fc1b897ee4f
Matěj Beran <matej.beran96@gmail.com>
2020-06-27 16:30:06 +0200

    Add deathFX and reload scene when player dies.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")] [SerializeField] float levelDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
+
     void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelDelay);
     }
 
     void StartDeathSequence()
     {
-        print("Player is dying");
         SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene() // string referenced
+    {
+        SceneManager.LoadScene(1);
     }
 }
